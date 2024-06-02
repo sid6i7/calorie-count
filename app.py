94576c0da5f1dae67563705efb8bd7daf4d034ef
Siddhant Jain <siddhantjain209@gmail.com>
2024-06-02 21:55:58 +0530

    add get_food endpoint to identify food

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def get_nutrition():
     model = Model()
     return jsonify(model.get_nutrition(path))
 
+@app.route('/get_food', methods=['POST'])
+def get_food():
+    file = request.files['image']
+    path = os.path.join(IMAGE_DIR, "temp.jpg")
+    file.save(path)
+    model = Model()
+    return jsonify(model.identify_food(path))
+
 if __name__ == "__main__":
     app.run(debug=True)
