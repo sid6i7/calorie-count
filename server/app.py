80352850cd6518af9bae66871b254b75e3c131c2
Siddhant Jain <siddhantjain209@gmail.com>
2024-06-02 23:07:23 +0530

    add cors to flask app

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from PIL import Image
 import os
 from constants import *
 from model.model import Model
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/get_nutrition', methods=['POST'])
 def get_nutrition():
